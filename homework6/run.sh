5755d5566d0ec22267f70055941f4c581aa12f67
Ilya Peresadin <pva701@gmail.com>
2015-03-18 03:37:49 +0300

    added script run test

---
+++ homework6/run.sh
@@ -0,0 +1,3 @@
+/usr/lib/jvm/jdk1.8.0_20/bin/java -cp IterativeParallelismTest.jar:/home/pva701/github/Java-Homework/libs/*:. info.kgeorgiy.java.advanced.concurrent.Tester list ru.ifmo.ctddev.peresadin.iterativeparallelism.IterativeParallelism
+
+
